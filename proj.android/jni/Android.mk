62585b4c94be7f51e949cb7bf7109b5b7ea8a1fb
Arthor <543294760@qq.com>
2015-04-24 14:56:46 +0800

    tests

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
 $(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/iconv)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
@@ -16,6 +17,9 @@ FILE_LIST := hellocpp/main.cpp
 FILE_LIST += $(wildcard $(LOCAL_PATH)/../../Classes/*.cpp)
 
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+#add support for iconv
+LOCAL_WHOLE_STATIC_LIBRARIES += android_support
 
 #LOCAL_SRC_FILES := hellocpp/main.cpp \
 #                   ../../Classes/AppDelegate.cpp \
@@ -31,13 +35,15 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
 
 
 LOCAL_STATIC_LIBRARIES := cocos2dx_static
-
+ 
 # _COCOS_LIB_ANDROID_BEGIN
 # _COCOS_LIB_ANDROID_END
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,.)
+#add support for iconv
+$(call import-module, android/support)
 
 # _COCOS_LIB_IMPORT_ANDROID_BEGIN
 # _COCOS_LIB_IMPORT_ANDROID_END
